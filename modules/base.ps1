b7e7d0d021919a681d7832d8ae5947895580149a
Aaron Jackson <ajackson002@dundee.ac.uk>
2023-07-06 10:57:41 +0100

    Create desktop shortcut to restart rclone

--- modules/base.ps1
+++ modules/base.ps1
@@ -5,3 +5,14 @@ Stop-Service -Name "wuauserv"
 
 # Fix Win Server 2019 Visual Style (hard to see window boarders)
 Set-ItemProperty -Path 'HKCU:\\Software\\Microsoft\\Windows\\DWM' -Name ColorPrevalence -Value 1
+
+# Create a desktop shortcut for remounting S3
+$batchPath = "C:\workdir\mount_s3.bat"
+New-Item $batchPath
+Set-Content $batchPath @"
+powershell.exe C:\workdir\start-rclone.ps1
+"@
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\Users\Administrator\Desktop\Remount D Drive.lnk")
+$Shortcut.TargetPath = $batchPath
+$Shortcut.Save()
