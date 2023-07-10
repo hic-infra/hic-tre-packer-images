a0373a8bbd02e4e20fff1581a32cce667d47b267
Aaron Jackson <ajackson002@dundee.ac.uk>
2023-07-10 22:36:45 +0100

    shellcheck

--- modules/conda-setup.sh
+++ modules/conda-setup.sh
@@ -15,10 +15,10 @@ MAMBAFORGE_SHA256=16c7d256de783ceeb39970e675efa4a8eb830dcbb83187f1197abfea0bf07d
 
 echo "Installing Conda"
 pushd /tmp
-curl -sfLO https://github.com/conda-forge/miniforge/releases/download/$MAMBAFORGE_VERSION/Mambaforge-$MAMBAFORGE_VERSION-Linux-x86_64.sh
+curl -sfLO "https://github.com/conda-forge/miniforge/releases/download/$MAMBAFORGE_VERSION/Mambaforge-$MAMBAFORGE_VERSION-Linux-x86_64.sh"
 echo "$MAMBAFORGE_SHA256 Mambaforge-$MAMBAFORGE_VERSION-Linux-x86_64.sh" | sha256sum -c -
-bash Mambaforge-$MAMBAFORGE_VERSION-Linux-x86_64.sh -b -f -p ~/conda
-rm -f Mambaforge-$MAMBAFORGE_VERSION-Linux-x86_64.sh
+bash "Mambaforge-$MAMBAFORGE_VERSION-Linux-x86_64.sh" -b -f -p ~/conda
+rm -f "Mambaforge-$MAMBAFORGE_VERSION-Linux-x86_64.sh"
 popd
 
 echo "Searching for conda environments"
@@ -30,7 +30,7 @@ for env in "${SETUPDIR}"/conda-environment*.yml ; do
 done
 
 echo "Setting default conda channel"
-cat > $HOME/.condarc <<EOF
+cat > "$HOME/.condarc" <<EOF
 channels:
   - conda-forge
 channel_alias: http://conda.hic-tre.dundee.ac.uk
--- modules/ssms.sh
+++ modules/ssms.sh
@@ -5,7 +5,7 @@ sudo dpkg -i azuredatastudio-linux-1.35.1.deb
 
 
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list \
+curl "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list" \
     | sudo tee /etc/apt/sources.list.d/mssql-release.list
 sudo apt-get update
 
