7657515d84e601e1854e421f1e9e8c95951bc860
Aaron Jackson <ajackson002@dundee.ac.uk>
2023-07-10 22:37:51 +0100

    LibreOffice URL updated

--- modules/libreoffice.ps1
+++ modules/libreoffice.ps1
@@ -3,8 +3,8 @@ $ProgressPreference = 'SilentlyContinue' # Disable (slow) progress bar
 
 Set-Location C:\Tools
 
-$LIBRE_VERSION = "7.5.2"
-$LIBRE_HASH    = "638e93876e8345666f1cf3e968373a90add5d420094a6ec8871edcda94a440e8"
+$LIBRE_VERSION = "7.5.4"
+$LIBRE_HASH    = "efca7b819427f709960437dc4f0c603d1a4f928493836781929f5472b376b864"
 
 $LIBRE_URL = "https://mirrors.ukfast.co.uk/sites/documentfoundation.org/tdf/libreoffice/stable/${LIBRE_VERSION}/win/x86_64/LibreOffice_${LIBRE_VERSION}_Win_x86-64.msi"
 
