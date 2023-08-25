b84fefe1b86091a306135115bcb08e4baccc2130
Aaron Jackson <ajackson002@dundee.ac.uk>
2023-08-25 08:22:36 +0100

    Bump RStudio for Ubuntu

--- modules/rstudio.sh
+++ modules/rstudio.sh
@@ -23,11 +23,11 @@ sudo apt install -y r-base
 # shellcheck source=/dev/null
 . /etc/lsb-release
 if [ "$DISTRIB_RELEASE" == "20.04" ] ; then
-    wget https://s3.amazonaws.com/rstudio-ide-build/desktop/bionic/amd64/rstudio-2022.07.2-576-amd64.deb
-    sudo gdebi -n rstudio-2022.07.2-576-amd64.deb
+    wget https://s3.amazonaws.com/rstudio-ide-build/electron/focal/amd64/rstudio-2023.06.1-524-amd64.deb
+    sudo gdebi -n rstudio-2023.06.1-524-amd64.deb
 elif [ "$DISTRIB_RELEASE" == "22.04" ] ; then
-    wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2022.12.0-353-amd64.deb
-    sudo gdebi -n rstudio-2022.12.0-353-amd64.deb
+    wget https://s3.amazonaws.com/rstudio-ide-build/electron/jammy/amd64/rstudio-2023.06.1-524-amd64.deb
+    sudo gdebi -n rstudio-2023.06.1-524-amd64.deb
 else
     echo "--------------------------------------------------------"
     echo "RStudio - Unsupported Ubuntu version ${DISTRIB_RELEASE}"
