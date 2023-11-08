154d84f9d0284e023daf097a737f12058dddd141
Aaron Jackson <ajackson002@dundee.ac.uk>
2023-11-08 09:18:58 +0000

    Pull stata testbuild (s3 fixes) from S3

--- modules/stata.ps1
+++ modules/stata.ps1
@@ -9,3 +9,9 @@ Start-Process SetupStata17.exe -ArgumentList "/s","/v`"/qn ADDLOCAL=core,StataMP
 # these. As far as as I understand, this lic file should work on both
 # Windows and Linux, but I think I prefer my expect solution...
 aws s3 cp s3://${Env:S3_PACKER_RESOURCES_PATH}/stata.lic "C:\\Program Files\\Stata17\\STATA.lic"
+
+# We have a temporary build of Stata 17 which fixes issues relating to
+# writing to rclone mounted (and OneDrive, apparently) volumes. This
+# will be included in a Stata 17 update at some point, but for now we
+# can pull down the test release.
+aws s3 cp s3://${Env:S3_PACKER_RESOURCES_PATH}/StataMP-64-test.exe "C:\\Program Files\\Stata17\\StataMP-64.exe"
