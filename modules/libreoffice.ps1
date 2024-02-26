e01ac0b7c291966cffa719855335b19035b7ce36
Simon Li <spli@dundee.ac.uk>
2024-02-26 13:09:32 +0000

    Update R versions

--- modules/libreoffice.ps1
+++ modules/libreoffice.ps1
@@ -3,8 +3,8 @@ $ProgressPreference = 'SilentlyContinue' # Disable (slow) progress bar
 
 Set-Location C:\Tools
 
-$LIBRE_VERSION = "7.6.3"
-$LIBRE_HASH    = "65a1330abaddf31e4dd8cb9c1110f422a3edc828b8d5680bcd3ea8efa508ef3c"
+$LIBRE_VERSION = "24.2.0"
+$LIBRE_HASH    = "58dcf13d87ef1279f7229b69949b836b73ce5f039ddb54cdf091f8fea28cc9f2"
 
 $LIBRE_URL = "https://mirrors.ukfast.co.uk/sites/documentfoundation.org/tdf/libreoffice/stable/${LIBRE_VERSION}/win/x86_64/LibreOffice_${LIBRE_VERSION}_Win_x86-64.msi"
 
