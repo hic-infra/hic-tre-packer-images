bdb2c9dac7cf682f5fe78528333f2f3151a964c5
Aaron Jackson <ajackson002@dundee.ac.uk>
2024-05-15 12:22:00 +0100

    Bump LibreOffice 24.2.3

--- modules/libreoffice.ps1
+++ modules/libreoffice.ps1
@@ -3,8 +3,8 @@ $ProgressPreference = 'SilentlyContinue' # Disable (slow) progress bar
 
 Set-Location C:\Tools
 
-$LIBRE_VERSION = "24.2.0"
-$LIBRE_HASH    = "58dcf13d87ef1279f7229b69949b836b73ce5f039ddb54cdf091f8fea28cc9f2"
+$LIBRE_VERSION = "24.2.3"
+$LIBRE_HASH    = "12b988318452120d31a4c618bc6ac314ad3bf99157584d0c2bf6029801a4cf31"
 
 $LIBRE_URL = "https://mirrors.ukfast.co.uk/sites/documentfoundation.org/tdf/libreoffice/stable/${LIBRE_VERSION}/win/x86_64/LibreOffice_${LIBRE_VERSION}_Win_x86-64.msi"
 
